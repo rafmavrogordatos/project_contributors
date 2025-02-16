09692deb181d6c98d6d2b1d2fde950fda4249280
Rafail Mavrogordatos <rafail.m@esofthall.gr>
2025-02-16 14:34:00 +0200

    removed unsed import

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Skill, Project, Application, Collaborator
 from django.contrib.auth.password_validation import validate_password
-from django.utils import timezone
+
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
